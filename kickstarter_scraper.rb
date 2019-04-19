366da596a9dad6f556b78a04a57c85b7cdfd61ad
David Cantoran <davidcantoran36@gmail.com>
2019-04-19 19:13:26 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,7 +5,13 @@ require 'nokogiri'
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
-  
+  result = {}
   kickstarter = Nokogiri::HTML(html)
-  {}
+  
+  kickstarter.each do ||
+    
+  end 
+  
+  result
+  
 end
